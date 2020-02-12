786fb520462cbcca45b4ffddb21ac4b0274e28d5
Vachounet <vachounet@live.fr>
2020-02-12 12:05:50 +0100

    Utils: add FAKE_PACKAGE_SIGNATURE to platform permissions
    [ TALU - renamed FAKE_PACKAGE permission group to FAKE_PACKAGE_SIGNATURE ]
    
    Change-Id: Ic00199f10de30c601acb9359ef292de9a150afa3
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- PermissionController/src/com/android/permissioncontroller/permission/utils/Utils.java
+++ PermissionController/src/com/android/permissioncontroller/permission/utils/Utils.java
@@ -21,6 +21,7 @@ import static android.Manifest.permission_group.CALENDAR;
 import static android.Manifest.permission_group.CALL_LOG;
 import static android.Manifest.permission_group.CAMERA;
 import static android.Manifest.permission_group.CONTACTS;
+import static android.Manifest.permission_group.FAKE_PACKAGE_SIGNATURE;
 import static android.Manifest.permission_group.LOCATION;
 import static android.Manifest.permission_group.MICROPHONE;
 import static android.Manifest.permission_group.NEARBY_DEVICES;
@@ -340,6 +341,8 @@ public final class Utils {
             PLATFORM_PERMISSIONS.put(Manifest.permission.POST_NOTIFICATIONS, NOTIFICATIONS);
             PLATFORM_PERMISSIONS.put(Manifest.permission.BODY_SENSORS_BACKGROUND, SENSORS);
         }
+
+        PLATFORM_PERMISSIONS.put(Manifest.permission.FAKE_PACKAGE_SIGNATURE, FAKE_PACKAGE_SIGNATURE);
 
         PLATFORM_PERMISSION_GROUPS = new ArrayMap<>();
         int numPlatformPermissions = PLATFORM_PERMISSIONS.size();
